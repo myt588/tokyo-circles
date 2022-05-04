2dc3575360aa1035c877780a00cb53a86bc6376a
myt588 <yetian.mao@gmail.com>
2022-05-04 23:57:04 +0800

    feat: add ramen project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'ラーメン博多福や',
+    description: `2022年落地日本连锁拉面店，从选址开始，设备购买，人员聘用全方位服务。实体店投入相对较大，但更加长线和稳定。`,
+    imgSrc: '/static/images/ramen.jpeg',
+    href: 'https://tabelog.com/tokyo/A1305/A130501/13255200',
+  },
+  {
     title: 'Pet Blog',
     description: `2021年落地的日本亚马逊电商项目。电商项目的投入相对较低，成本可控，办公灵活自由。`,
     imgSrc: '/static/images/pet-blog.png',
