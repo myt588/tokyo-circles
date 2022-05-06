d2c0c964d86211df5e3e2f7f5cd3fe0330dd1257
myt588 <yetian.mao@gmail.com>
2022-05-06 21:43:31 +0800

    feat: update two blogs for official release

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,13 +7,13 @@ export default function Projects() {
   return (
     <>
       <PageSEO
-        title={`已落地项目-东京圈-做日本最透明的投资移民生活信息平台 - ${siteMetadata.author}`}
+        title={`合作落地项目-东京圈-做日本最透明的投资移民生活信息平台 - ${siteMetadata.author}`}
         description={siteMetadata.description}
       />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            已落地项目
+            合作落地项目
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             目前东京圈的客户所落地的项目
