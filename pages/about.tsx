6a06c523ea957dea814333c5342b23936d0f8c04
myt588 <yetian.mao@gmail.com>
2022-06-01 17:22:44 +0800

    feat: add redbook qrcode to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,4 @@
+import Image from '../components/Image'
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { InferGetStaticPropsType } from 'next'
 import { allAuthors } from 'contentlayer/generated'
@@ -10,5 +11,21 @@ export const getStaticProps = async () => {
 }
 
 export default function About({ author }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <MDXLayoutRenderer layout={author.layout || DEFAULT_LAYOUT} content={author} />
+  return (
+    <>
+      <MDXLayoutRenderer layout={author.layout || DEFAULT_LAYOUT} content={author} />
+      <div className="flex justify-center pt-3">
+        <div>
+          <Image
+            alt="小红书二维码"
+            src={'/static/images/redbook.jpg'}
+            className="object-cover object-center md:h-36 lg:h-48"
+            width={200}
+            height={200}
+          />
+          <div className="text-center">关注我们的小红书</div>
+        </div>
+      </div>
+    </>
+  )
 }
